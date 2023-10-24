9b8846248b5f5f7e420c1a07a1e978654ce29e28
Arsla-1 <105283537+Arsla-1@users.noreply.github.com>
2023-10-24 15:16:49 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        // cream: "#f0e3ce",
+        cream: "#f2eae2",
+        secondary: "rgb(54, 52, 52)",
+        accent: "#3d8168",
+        darkaccent: "#1a4032",
+        muted: "#6c7289",
+      },
+      fontFamily: {
+        fraunces: ["Fraunces", "serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      fontSize: {
+        heading: "2rem",
+      },
+    },
+  },
+  plugins: [],
+};
